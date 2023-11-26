4161ac7da62469c8100c8c2d63d78d9321e708b3
MauroMauro <blur.benitez@gmail.com>
2023-11-26 18:05:50 -0300

    implemented CER-UVA api

--- cotizaciones-e-indices/src/componentes/Footer.js
+++ cotizaciones-e-indices/src/componentes/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
     return (
         <div>
-            <Container className="container-footer" bg="dark" sticky="bottom">
+            <Container className="container-footer" bg="dark">
                 German Benitez 2023
             </Container>            
         </div>
--- cotizaciones-e-indices/src/componentes/PanelCotizaciones.js
+++ cotizaciones-e-indices/src/componentes/PanelCotizaciones.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 
 import servicioDolar from '../servicios/dolar'
+import servicioUva from '../servicios/uva'
+import servicioCer from '../servicios/cer'
 
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -13,11 +15,23 @@ import BloqueInputCotizaciones from './BloqueInputCotizaciones'
 
 const PanelCotizaciones = () => {
     const [cotizacionesDolar, setCotizacionesDolar] = useState([])
+    const [cotizacionesUva, setCotizacionesUva] = useState([])
+    const [cotizacionesCer, setCotizacionesCer] = useState([])
 
     useEffect(() => {
         servicioDolar.getAll()
             .then(cotizacionesDeLaApi => setCotizacionesDolar(cotizacionesDeLaApi))
-    },[])
+    }, [])
+
+    useEffect(() => {
+        servicioUva.getAll()
+            .then(cotizacionesDeLaApiUva => setCotizacionesUva(cotizacionesDeLaApiUva))
+    }, [])
+
+    useEffect(() => {
+        servicioCer.getAll()
+            .then(cotizacionesDeLaApiCer => setCotizacionesCer(cotizacionesDeLaApiCer))
+    }, [])
 
     const [montoArs, setMontoArs] = useState(0)
     const [montoUsd, setMontoUsd] = useState(0)
@@ -30,19 +44,23 @@ const PanelCotizaciones = () => {
     const handleShowSolidario = () => setShowSolidario(true)
     const [showMep, setShowMep] = useState(false)
     const handleShowMep = () => setShowMep(true)
-    
+    const [showCer, setShowCer] = useState(false)
+    const handleShowCer = () => setShowCer(true)
+    const [showUva, setShowUva] = useState(false)
+    const handleShowUva = () => setShowUva(true)
+
 
     return (
         <div className='bg-dark text-center'>
 
             <Container className='bg-dark' >
-                <BloqueInputCotizaciones monto={montoArs} setMonto={setMontoArs} moneda={"ARS"}/> 
-                <BloqueInputCotizaciones monto={montoUsd} setMonto={setMontoUsd} moneda={"USD"}/> 
+                <BloqueInputCotizaciones monto={montoArs} setMonto={setMontoArs} moneda={"ARS"} />
+                <BloqueInputCotizaciones monto={montoUsd} setMonto={setMontoUsd} moneda={"USD"} />
             </Container>
 
             <Container className='bg-dark' >
                 <Row bg="dark" data-bs-theme="dark" className='my-2'>
-                    
+
                     <Col bg="dark" md={3}>
                         <Card className="card-dolar py-4 my-2" bg="dark" onClick={handleShowOficial} >
                             <CardTitle>
@@ -53,21 +71,21 @@ const PanelCotizaciones = () => {
                             </CardTitle>
                             <CardFooter>
                                 Sus <b>ARS</b> en tenencia equivalen a<br></br>
-                                <span className='monto'>{(montoArs/(cotizacionesDolar.oficial)).toFixed(2)}</span> USD
+                                <span className='monto'>{(montoArs / (cotizacionesDolar.oficial)).toFixed(2)}</span> USD
                                 <br></br>
                                 <br></br>
                                 Sus <b>USD</b> intercambiados a precio oficial equivalen a <br></br>
-                                <span className='montoUsd'>{(montoUsd*(cotizacionesDolar.oficial)).toFixed(0)}</span> ARS
+                                <span className='montoUsd'>{(montoUsd * (cotizacionesDolar.oficial)).toFixed(0)}</span> ARS
                             </CardFooter>
                         </Card>
                     </Col>
-                    <ModalDesplegable 
-                        titulo="Dolar Oficial" 
-                        texto="Es el valor del dólar que se liquida por parte del gobierno nacional" 
-                        show={showOficial} 
-                        setShow={setShowOficial}  
+                    <ModalDesplegable
+                        titulo="Dolar Oficial"
+                        texto="Es el valor del dólar que se liquida por parte del gobierno nacional"
+                        show={showOficial}
+                        setShow={setShowOficial}
                     />
-                    
+
 
 
                     <Col md={3}>
@@ -80,19 +98,19 @@ const PanelCotizaciones = () => {
                             </CardTitle>
                             <CardFooter>
                                 Sus <b>ARS</b> en tenencia equivalen a<br></br>
-                                <span className='monto'>{(montoArs/(cotizacionesDolar.blue)).toFixed(2)}</span> USD
+                                <span className='monto'>{(montoArs / (cotizacionesDolar.blue)).toFixed(2)}</span> USD
                                 <br></br>
                                 <br></br>
                                 Sus <b>USD</b> intercambiados a precio dolar blue equivalen a <br></br>
-                                <span className='montoUsd'>{(montoUsd*(cotizacionesDolar.blue)).toFixed(0)}</span> ARS
+                                <span className='montoUsd'>{(montoUsd * (cotizacionesDolar.blue)).toFixed(0)}</span> ARS
                             </CardFooter>
                         </Card>
                     </Col>
-                    <ModalDesplegable 
-                        titulo="Dolar Blue" 
-                        texto="Es el valor del dólar que se paga en el mercado paralelo" 
-                        show={showBlue} 
-                        setShow={setShowBlue}  
+                    <ModalDesplegable
+                        titulo="Dolar Blue"
+                        texto="Es el valor del dólar que se paga en el mercado paralelo"
+                        show={showBlue}
+                        setShow={setShowBlue}
                     />
 
 
@@ -107,20 +125,20 @@ const PanelCotizaciones = () => {
                             </CardTitle>
                             <CardFooter>
                                 Sus <b>ARS</b> en tenencia equivalen a<br></br>
-                                <span className='monto'>{(montoArs/(cotizacionesDolar.solidario)).toFixed(2)}</span> USD
+                                <span className='monto'>{(montoArs / (cotizacionesDolar.solidario)).toFixed(2)}</span> USD
                                 <br></br>
                                 <br></br>
                                 Sus <b>USD</b> intercambiados a precio dolar solidario equivalen a <br></br>
-                                <span className='montoUsd'>{(montoUsd*(cotizacionesDolar.solidario)).toFixed(0)}</span> ARS
+                                <span className='montoUsd'>{(montoUsd * (cotizacionesDolar.solidario)).toFixed(0)}</span> ARS
                             </CardFooter>
                         </Card>
-                        
+
                     </Col>
-                    <ModalDesplegable 
-                        titulo="Dolar Solidario" 
-                        texto="Es el valor del dolar oficial al que se le suman los cargos impuestos por el gobierno a todas las operaciones de compra de moneda extranjera por canales oficiales" 
-                        show={showSolidario} 
-                        setShow={setShowSolidario}  
+                    <ModalDesplegable
+                        titulo="Dolar Solidario"
+                        texto="Es el valor del dolar oficial al que se le suman los cargos impuestos por el gobierno a todas las operaciones de compra de moneda extranjera por canales oficiales"
+                        show={showSolidario}
+                        setShow={setShowSolidario}
                     />
 
 
@@ -134,21 +152,56 @@ const PanelCotizaciones = () => {
                             </CardTitle>
                             <CardFooter>
                                 Sus <b>ARS</b> en tenencia equivalen a<br></br>
-                                <span className='monto'>{(montoArs/(cotizacionesDolar.mep)).toFixed(2)}</span> USD
+                                <span className='monto'>{(montoArs / (cotizacionesDolar.mep)).toFixed(2)}</span> USD
                                 <br></br>
                                 <br></br>
                                 Sus <b>USD</b> intercambiados a precio MEP equivalen a <br></br>
-                                <span className='montoUsd'>{(montoUsd*(cotizacionesDolar.mep)).toFixed(0)}</span> ARS
+                                <span className='montoUsd'>{(montoUsd * (cotizacionesDolar.mep)).toFixed(0)}</span> ARS
                             </CardFooter>
                         </Card>
                     </Col>
-                    <ModalDesplegable 
-                        titulo="Dolar MEP" 
-                        texto="El dólar MEP o Dólar Bolsa es aquel tipo de cambio resultante de una operación de compra de elementos en pesos y su posterior venta en dólares" 
-                        show={showMep} 
-                        setShow={setShowMep}  
+                    <ModalDesplegable
+                        titulo="Dolar MEP"
+                        texto="El dólar MEP o Dólar Bolsa es aquel tipo de cambio resultante de una operación de compra de elementos en pesos y su posterior venta en dólares"
+                        show={showMep}
+                        setShow={setShowMep}
                     />
 
+                </Row>
+                <Row bg="dark" data-bs-theme="dark" className='my-2'>
+                    <Col bg="dark" md={6}>
+                        <Card className="card-dolar py-4 my-2" bg="dark" onClick={handleShowCer} >
+                            <CardTitle>
+                                Coeficiente CER
+                            </CardTitle>
+                            <CardTitle className='display-5'>
+                                {cotizacionesCer.value}
+                            </CardTitle>
+                        </Card>
+                    </Col>
+                    <ModalDesplegable
+                        titulo="Coeficiente CER"
+                        texto="El Coeficiente de Estabilización de Referencia (CER) tiene como objetivo reflejar la inflación minorista. Se computa a partir de la tasa de variación promedio-diario del Índice de Precios al Consumidor (IPC) correspondiente al mes previo."
+                        show={showCer}
+                        setShow={setShowCer}
+                    />
+
+                    <Col bg="dark" md={6}>
+                        <Card className="card-dolar py-4 my-2" bg="dark" onClick={handleShowUva} >
+                            <CardTitle>
+                                Índice UVA
+                            </CardTitle>
+                            <CardTitle className='display-5'>
+                                {cotizacionesUva.value}
+                            </CardTitle>
+                        </Card>
+                    </Col>
+                    <ModalDesplegable
+                        titulo="Índice UVA"
+                        texto="La Unidad de Valor Adquisitivo es una medida que equivale a la milésima parte del costo promedio del metro cuadrado de la vivienda a construir. Por ejemplo, 1000 UVA alcanzan para construir un metro cuadrado en cualquier momento futuro."
+                        show={showUva}
+                        setShow={setShowUva}
+                    />
                 </Row>
             </Container>
         </div>
