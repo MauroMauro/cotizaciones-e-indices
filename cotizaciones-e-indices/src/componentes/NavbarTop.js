80f31870ac1df1133438a58985946843d2617435
MauroMauro <blur.benitez@gmail.com>
2023-12-03 15:53:15 -0300

    deploy config

--- cotizaciones-e-indices/src/componentes/NavbarTop.js
+++ cotizaciones-e-indices/src/componentes/NavbarTop.js
@@ -16,8 +16,8 @@ const NavbarTop = () => (
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Nav className="me-auto">
-            <Nav.Link href="http://localhost:3000">Cotizaciones</Nav.Link>
-            <Nav.Link href="http://localhost:3000/calculadora">Calculadora Rulo</Nav.Link>
+            <Nav.Link href="https://mauromauro.github.io/cotizaciones-e-indices/">Cotizaciones</Nav.Link>
+            <Nav.Link href="https://mauromauro.github.io/cotizaciones-e-indices/calculadora">Calculadora Rulo</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
